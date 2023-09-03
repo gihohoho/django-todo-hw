d346713e78eb203dd48aacd8a95e7369646bfda8
USER\HOME <leekh9997@naver.com>
2023-09-03 13:30:42 +0900

    add user model

--- todoproject/views.py
+++ todoproject/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'index.html')
+
+def getdata(request):
+    print(request.POST.get('todo'))
+    return HttpResponse('ok')
