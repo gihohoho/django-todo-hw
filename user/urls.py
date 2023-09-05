0fa44f1d4794c0f67d6fefbeaae7077d1bd9ba28
USER\HOME <leekh9997@naver.com>
2023-09-05 17:28:29 +0900

    login,logout

--- user/urls.py
+++ user/urls.py
@@ -19,5 +19,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('signup/', views.signup)
+    path('signup/', views.signup),
+    path('login/', views.login),
+    path('logout/', views.logout),
 ]
